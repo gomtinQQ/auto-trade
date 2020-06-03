7c953cba700b296debb99492cdc9aaf78ddbe16a
steve8618 <modu.stevelee@gmail.com>
2020-06-03 20:58:57 +0900

    not uploaded

--- auto-trade/kiwoom/kiwoom.py
+++ auto-trade/kiwoom/kiwoom.py
@@ -245,7 +245,7 @@ class Kiwoom(QAxWidget):
             self.event.exit()
 
     def kiwoom_tr_recall(self, tr_name, tr_code, screen_no, pre_next):
-        time.sleep(3)
+        time.sleep(3.6)
 
         log.instance().logger().debug("연속조회")
         if tr_code.lower() == 'OPT10081'.lower():
